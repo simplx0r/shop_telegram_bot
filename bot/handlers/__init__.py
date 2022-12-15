d0e43b349640a542768f834e4ce446efa6ef787c
Danil <3483211@gmail.com>
2022-12-15 16:40:59 +0700

    simplify structure

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -30,26 +30,15 @@ def register_user_commands(router: Router) -> None:
     :param router:
     """
     router.message.register(start, CommandStart())
-    router.message.register(help_command, Command(commands=['help']))
+
     router.message.register(start, F.text == __('Старт'))
     router.message.register(menu_posts, F.text == __('Твои посты'))
     router.message.register(menu_account, F.text == __('Аккаунт'), F.content_type == [ContentType.CONTACT, ])
     router.message.register(menu_channels, F.text == __('Твои каналы'))
-    router.message.register(menu_posts_create_text, PostStates.waiting_for_text)
-    router.message.register(menu_posts_create_subs_min, PostStates.waiting_for_post_subs_min)
-    router.message.register(menu_posts_create_prtype_url, PostStates.waiting_for_price_url)
-    router.message.register(menu_posts_create_prtype_pub, PostStates.waiting_for_price_publication)
-    router.message.register(menu_posts_create_prtype, PostStates.waiting_for_pr_type)
-    router.message.register(menu_posts_create_url,
-                            PostStates.waiting_for_url)
-    router.message.register(help_func, F.text == __('Помощь'))
 
-    router.callback_query.register(call_help_func, F.data == 'help', any_state)
-    router.callback_query.register(call_start, F.data == 'menu', any_state)
-    router.callback_query.register(menu_posts_create,
-                                   PostCD.filter(F.action == PostCDAction.CREATE), PostStates.waiting_for_select)
-    router.callback_query.register(menu_posts_get,
-                                   PostCD.filter(F.action == PostCDAction.GET), PostStates.waiting_for_select)
+
+
+
 
 
 # Alias
